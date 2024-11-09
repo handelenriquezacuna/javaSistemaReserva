42204cc12f97dbd0fdf12d35e815c652a45513f5
HandelEnriquez <handell1210@gmail.com>
2024-11-08 18:57:15 -0600

    Subiendo Infrastructura de variables y funcion para ingresar empleados

--- sistemareservajava/src/sistemareservajava/Barista.java
+++ sistemareservajava/src/sistemareservajava/Barista.java
@@ -4,22 +4,92 @@
  */
 package sistemareservajava;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author handel enriquez
  */
 
 public class Barista {
+    private String baristaNuevo = "";
+    private String empleado = "";
+    private int numEmpleado = 0;
+    private EnumMenu menu;
+    private int numBebida;
+    
+    public void ingresarEmpleados(){
+        /*
+        Ingreso de Empleados
+        */
+        this.numEmpleado = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de empleados"));
+        for (int i = 0; i < this.numEmpleado; i++) {
+            this.empleado = JOptionPane.showInputDialog("Ingrese el nombre del empleado");
+        }
+    }    
+    
+    /*
+    Constructores empiezan aca 
+    */
+    public Barista() {
+    
+    }
+    
+    public Barista(String baristaNuevo, String empleado, int numEmpleado, EnumMenu menu) {
+        this.baristaNuevo = baristaNuevo;
+        this.empleado = empleado;
+        this.numEmpleado = numEmpleado;
+        this.menu = menu;
+    }
 
-    // TODO: Implementar constructor para inicializar el sistema de pedidos del barista
-    // public Barista() { ... }
+    /*
+    Terminan Constructores 
+    */
+    
+    /*
+    Getters y Setters para la extraccion 
+    */
+    
+    public int getNumBebida() {
+        return numBebida;
+    }
+    
+    public EnumMenu getMenu() {
+        return menu;
+    }
 
-    // TODO: Implementar método para que un empleado reserve una bebida (validar que no haya pedido otra antes)
-    // public boolean reservarBebida(String idEmpleado, String bebida) { ... }
+    public void setMenu(EnumMenu menu) {
+        this.menu = menu;
+    }  
 
-    // TODO: Implementar método para modificar un pedido de bebida existente
-    // public void modificarPedido(String idEmpleado, String nuevaBebida) { ... }
+    public int getNumEmpleado() {
+        return numEmpleado;
+    }
 
-    // TODO: Implementar método para listar todos los pedidos realizados
-    // public void listarPedidos() { ... }
+    public void setNumEmpleado(int numEmpleado) {
+        this.numEmpleado = numEmpleado;
+    }     
+        
+    public String getBaristaNuevo() {
+        return baristaNuevo;
+    }
+
+    public void setBaristaNuevo() {
+        this.baristaNuevo = JOptionPane.showInputDialog("Ingrese el nombre del Barista");
+    }
+
+    public String getEmpleado() {   
+        return empleado;
+    }
+
+    public void setEmpleado(String empleado) {
+        this.empleado = empleado;
+    }
+
+    
+    /*  
+    Terminan Getters y Setters para la extraccion 
+    */
+
+    
 }
---
+++ sistemareservajava/src/sistemareservajava/EnumMenu.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package sistemareservajava;
+
+/**
+ *
+ * @author hande
+ */
+public enum EnumMenu {
+    CafeNormal,Capuchino,CapuchinoVainilla,Chocolate,Moka,TeChai,CafeFrio
+}
